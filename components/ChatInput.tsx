d6e0a155330ea0d25bf28fbae28dbc92013f6d18
vmeset <vmeset@gmail.com>
2023-02-15 15:32:15 +0300

    chatGPT api

--- components/ChatInput.tsx
+++ components/ChatInput.tsx
@@ -1,8 +1,11 @@
 'use client'
 
 import { PaperAirplaneIcon } from "@heroicons/react/24/outline"
+import { addDoc, collection, serverTimestamp } from "firebase/firestore"
 import { useSession } from "next-auth/react"
-import { useState } from "react"
+import { FormEvent, useState } from "react"
+import { toast } from "react-hot-toast"
+import { db } from "../firebase"
 
 interface ChatInputProps {
   chatId: string
@@ -12,13 +15,55 @@ function ChatInput({chatId}: ChatInputProps) {
   
   const [text, setText] = useState('')
   const {data: session} = useSession()
-  const submitHandler = () => {
 
+  // useSWR to get model
+  const model = 'text-davinci-003'
+
+  const submitHandler = async (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault()
+    if(!text) return
+
+    const input = text.trim()
+    setText('')
+
+    const message: Message = {
+      text: input,
+      createdAt: serverTimestamp(),
+      user: {
+        _id: session?.user?.email!,
+        name: session?.user?.name!,
+        avatar: session?.user?.image! || '/no_image.png'
+      }
+    }
+
+    await addDoc(
+      collection(db, 'users', session?.user?.email!, 'chats', chatId, 'messages'),
+      message
+    )
+
+    const notification = toast.loading('Думаю...')
+
+    await fetch('/api/askQuestion', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        prompt: input, 
+          chatId, 
+          model, 
+          session,
+      })
+    }).then(() => {
+      toast.success('Ответ получен', {
+        id: notification
+      })
+    })
   }
 
   return (
     <div className="bg-gray-700/50 text-gray-400 rounded-lg text-sm outline-none">
-      <form action="" className="p-6 space-x-3 flex" onSubmit={submitHandler}>
+      <form className="p-6 space-x-3 flex" onSubmit={submitHandler}>
         <input className="bg-transparent focus:outline-none flex-1 disabled:cursor-not-allowed disabled:text-gray-300" disabled={!session}
         type="text" value={text} onChange={e => setText(e.target.value)}
           placeholder="Напиши уже что-нибудь.."
---
+++ pages/api/askQuest.tsx
@@ -0,0 +1,26 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import type { NextApiRequest, NextApiResponse } from 'next'
+
+type Data = {
+  answer: string
+}
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<Data>
+) {
+  const {prompt, chatId, model, session} = req.body
+
+  if(!prompt) {
+    res.status(400).json({answer: 'Отправь запрос'})
+    return
+  }
+  if(!chatId) {
+    res.status(400).json({answer: 'Некорректный id у чата'})
+    return
+  }
+
+  const response = await query(prompt, chatId, model)
+
+  res.status(200).json({ name: 'John Doe' })
+}
